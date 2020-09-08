2377efe43527fb31182f1bc2138df45d282d8a5c
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-09-08 11:16:04 -0300

    Funcionalidade: Adição de gráficos.

--- aula24.js
+++ aula24.js
@@ -12,6 +12,9 @@ fetch("https://extreme-ip-lookup.com/json/")
         .then((jsonCOVID) => {
             document.querySelector(".casos").innerText = jsonCOVID.results[0].confirmed;
 
+            const datasetDatas = [];
+            const datasetCasos = [];
+
             for (const item of jsonCOVID.results) {
                 const elemento = document.createElement("li");
                 const dia = document.createElement("div");
@@ -31,10 +34,53 @@ fetch("https://extreme-ip-lookup.com/json/")
                 elemento.append(dia);
                 elemento.append(casos);
                 dados.append(elemento);
+            
+                
+
+                datasetDatas.push(dia.innerText);
+                datasetCasos.push(casos.innerText);
             }
+
+            const dataset = [];
+            dataset.push(datasetDatas.reverse());
+            dataset.push(datasetCasos.reverse());
+            return dataset;
+        })
+        .then((dataset) => {
+
+            const casosDiarios = dataset[1].map((item, i, array) => {
+                return item - array[i-1];
+            })
+            casosDiarios[0] = dataset[1][0];
+
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: dataset[0],
+                    datasets: [{
+                        label: 'Casos por Dia',
+                        data: casosDiarios,
+                        backgroundColor: ['#7987d9']
+                    }]
+                },
+            });
+
+            const myChart2 = new Chart(ctx2, {
+                type: 'line',
+                data: {
+                    labels: dataset[0],
+                    datasets: [{
+                        label: 'Casos',
+                        data: dataset[1],
+                        backgroundColor: ['#7987d9']
+                    }]
+                },
+            });
         })
 
 const dados = document.querySelector(".dados");
+const ctx = document.getElementById('myChart');
+const ctx2 = document.getElementById('myChart2');
 
 const transformaEstado = (estado) => {
     
@@ -70,3 +116,4 @@ const transformaEstado = (estado) => {
 
     return estadoSigla[estado];
 }
+
